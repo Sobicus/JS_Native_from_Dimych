c7a40b58665dad1a743199630812c402f58a3bcf
Maksym_Vasylenko <zvolandz@gmail.com>
2022-04-14 00:41:45 +0200

    test1

---
+++ src/01_tests/01.ts
@@ -0,0 +1,10 @@
+export function  sum(a: number, b: number) {
+    return a+ b;
+}
+export function  mult(a: number, b: number) {
+    return a* b;
+}
+
+export function splitIntoWords(sentense: string){
+    return ['hello', 'my', 'friend']
+}
